e77ae7e214e541e184ae7d75ab9c9c531343f4c8
Alex Talreja <alextalreja@gmail.com>
2023-05-23 16:50:41 -0400

    quickFix

--- school-app/src/components/Firebase.js
+++ school-app/src/components/Firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDhDFcNTl1vIfS5IY7fie-KEJv28d7IEKs",
-  authDomain: "team1tjes.firebaseapp.com",
-  projectId: "team1tjes",
-  storageBucket: "team1tjes.appspot.com",
-  messagingSenderId: "975825422274",
-  appId: "1:975825422274:web:8aacc0369b2354e550785b"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
