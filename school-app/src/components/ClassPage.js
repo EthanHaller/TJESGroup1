c8dd314b5efdc9340296453f6e2ce8970a2245be
averydossman <averydossman@gmail.com>
2023-05-24 16:40:29 -0400

    Added styling

--- school-app/src/components/ClassPage.js
+++ school-app/src/components/ClassPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
 import db from '../Firebase';
-import { Grid, TextField, Button, Typography, Container } from '@mui/material';
+import { Box, Card, CardContent, CardActions, TextField, Button, Typography, Container, Grid, Paper } from '@mui/material';
 import { useParams } from 'react-router-dom';
 
 
@@ -15,7 +15,7 @@ function ClassPage() {
   // Use a specific class ID
   const { classId } = useParams();
 
-  
+
 
   useEffect(() => {
     const fetchClassData = async () => {
@@ -26,12 +26,12 @@ function ClassPage() {
         const data = classDataSnapshot.data();
         setClassData(data);
 
-        const studentsPromises = data.roster.map((studentId) => getDoc(doc(db, 'students', studentId)));
+        const studentsPromises = data.roster?.map((studentId) => getDoc(doc(db, 'students', studentId))) || [];
         const studentsSnapshots = await Promise.all(studentsPromises);
 
         const studentsData = studentsSnapshots.map(snapshot => ({
           ...snapshot.data(),
-          grade: snapshot.data().grades[data.subject], // replaced classId with data.subject
+          grade: snapshot.data().grades?.[data.subject], // Make sure grades and subject exist
         }));
         setStudentsData(studentsData);
       } else {
@@ -41,6 +41,7 @@ function ClassPage() {
 
     fetchClassData();
   }, [classId]);
+
 
 
   const handleEditClick = (index) => {
@@ -56,9 +57,9 @@ function ClassPage() {
       const newStudentsData = [...studentsData];
       newStudentsData[editIndex].name = editValue.name;
       newStudentsData[editIndex].grade = editValue.grade;
-  
+
       const studentId = classData.roster[editIndex];
-  
+
       await setDoc(doc(db, 'students', studentId), {
         name: editValue.name,
         grades: {
@@ -66,7 +67,7 @@ function ClassPage() {
           [classData.subject]: editValue.grade,
         },
       });
-  
+
       // Update the local state with the updated student data
       setStudentsData(newStudentsData);
       setEditIndex(-1);
@@ -84,44 +85,52 @@ function ClassPage() {
   }
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-      <h2>{classData.name}</h2>
-      <div>
-      <h2>Class Page</h2>
-        <Typography variant="h6">Subject: {classData.subject}</Typography>
-        <Typography variant="h6">Grade: {classData.grade}</Typography>
-        <Typography variant="h6">Teacher: {classData.teacher}</Typography>
-        <Typography variant="h6">Class Size: {classData.num}</Typography>
-      </div>
-      {studentsData && studentsData.length > 0 &&
-        <div style={{ marginTop: '2rem' }}>
-          <Container>
-            <Typography variant="h5" style={{textAlign: 'center' }}>Roster:</Typography>
-            <Grid container spacing={3} style={{ marginTop: '1rem', maxWidth: '500px', margin: 'auto' }}>
-              <Grid item xs={4}><Typography variant="h6">Name</Typography></Grid>
-              <Grid item xs={4}><Typography variant="h6">Grade</Typography></Grid>
-              <Grid item xs={4}><Typography variant="h6">Action</Typography></Grid>
+    <Container maxWidth="md">
+      <Box my={4}>
+        <Card elevation={3}>
+          <CardContent>
+            <Typography variant="h4" component="div" gutterBottom>{classData.name}</Typography>
+            <Typography variant="h6" component="div"><b>Subject:</b> {classData.subject}</Typography>
+            <Typography variant="h6" component="div"><b>Grade:</b> {classData.grade}</Typography>
+            <Typography variant="h6" component="div"><b>Teacher:</b> {classData.teacher}</Typography>
+            <Typography variant="h6" component="div"><b>Class Size:</b> {classData.num}</Typography>
+          </CardContent>
+        </Card>
+        <Box my={4}>
+          <Paper elevation={3} sx={{ padding: 2 }}>
+            <Typography variant="h5" gutterBottom><b>Roster</b></Typography>
+            <Grid container spacing={2} alignItems="center">
+              <Grid item xs={4}><Typography variant="h6" style={{ textDecoration: 'underline' }}>Name</Typography></Grid>
+              <Grid item xs={4}><Typography variant="h6" style={{ textDecoration: 'underline' }}>Grade</Typography></Grid>
+              <Grid item xs={4}><Typography variant="h6" style={{ textDecoration: 'underline' }}>Action</Typography></Grid>
+
               {studentsData.map((student, index) => (
                 editIndex === index ? (
                   <React.Fragment key={index}>
-                    <Grid item xs={4}><TextField value={editValue.name} onChange={(e) => handleEditChange('name', e.target.value)} label="Name" /></Grid>
-                    <Grid item xs={4}><TextField value={editValue.grade} onChange={(e) => handleEditChange('grade', e.target.value)} label="Grade" /></Grid>
-                    <Grid item xs={4}><Button variant="contained" onClick={handleSaveClick}>Save</Button></Grid>
+                    <Grid item xs={4}>
+                      <TextField value={editValue.name || ''} onChange={(e) => handleEditChange('name', e.target.value)} label="Name" />
+                    </Grid>
+                    <Grid item xs={4}>
+                      <TextField value={editValue.grade || ''} onChange={(e) => handleEditChange('grade', e.target.value)} label="Grade" />
+                    </Grid>
+                    <Grid item xs={4}>
+                      <Button variant="contained" color="primary" onClick={handleSaveClick}>Save</Button>
+                    </Grid>
                   </React.Fragment>
                 ) : (
                   <React.Fragment key={index}>
-                    <Grid item xs={4}><Typography variant="body1">{student.name}</Typography></Grid>
-                    <Grid item xs={4}><Typography variant="body1">{student.grade}</Typography></Grid>
+                    <Grid item xs={4}><Typography variant="body1">{student.name || 'N/A'}</Typography></Grid>
+                    <Grid item xs={4}><Typography variant="body1">{student.grade || 'N/A'}</Typography></Grid>
                     <Grid item xs={4}><Button variant="contained" onClick={() => handleEditClick(index)}>Edit</Button></Grid>
                   </React.Fragment>
                 )
               ))}
             </Grid>
-          </Container>
-        </div>
-      }
-    </div>
+          </Paper>
+        </Box>
+      </Box>
+    </Container>
   );
-  }
+}
 
 export default ClassPage;
