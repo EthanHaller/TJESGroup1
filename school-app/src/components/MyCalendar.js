c77296ff4ce25a5d30fa3aa3078f4f944cf19262
yash-d99 <dubey.yash2@gmail.com>
2023-05-24 17:08:40 -0400

    final working calendar

--- school-app/src/components/MyCalendar.js
+++ school-app/src/components/MyCalendar.js
@@ -1,26 +1,68 @@
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import Box from '@mui/material/Box'
+import db from '../Firebase'
+import { deleteDoc, getDocs, query, addDoc, collection , doc, where} from "firebase/firestore";
+
 function MyCalendar(props) {
 
-  // const handleAddEventsClick = () => {
+  function refresh() {
+    window.location.reload(false);
+  }
+  const handleAddEventsClick = () => {
+    const titleResponse = prompt('What is the title of your event?');
+    const startResponse = prompt('What is the start date of your event? (YYYY-MM-DD)')
+    const endResponse = prompt('What is the end date of your event? (YYYY-MM-DD)')
+    if(titleResponse && startResponse && endResponse) {
+      addDoc(collection(db, 'calendar'), {
+        title: titleResponse,
+        start: startResponse,
+        end: endResponse
+      })
+      .then(() => {
+        setTimeout(refresh(), 3000);
+      });
+    }
 
-  // }
+  }
+  const handleDeleteEventsClick =  async () => {
+    const titleResponse = prompt('What is the title of the event you want to delete?');
+    if(titleResponse) {
+      const q = query(collection(db, "calendar"), where("title", "==", titleResponse));
+
+      const querySnapshot =  await getDocs(q);
+      querySnapshot.forEach((document) => {
+      deleteDoc(doc(db, 'calendar', document.id))
+      .then(() => {
+        setTimeout(refresh(), 3000);
+      });
+   });
+  }
+}
     return (
        <Box>
           <FullCalendar
+          
           plugins={[ dayGridPlugin ]}
           events = {props.event_array}
           initialView="dayGridMonth"
           headerToolbar = {{
-            left: "prev,next",
+            left: "prevYear,prev,next,nextYear",
             center: "title",
-            right: "dayGridMonth"
+            right: "dayGridDay,dayGridWeek,dayGridMonth add,delete"
           }}
-          buttonText={{
-            dayGridMonth: 'Show events',
+          customButtons={{
+            add: {
+              text: 'Add Event',
+              click: handleAddEventsClick,
+              className: 'add-event-button'
+            },
+            delete: {
+              text: 'Delete Event',
+              click: handleDeleteEventsClick,
+              
+            }
           }}
-          
           />
       </Box> 
     );
