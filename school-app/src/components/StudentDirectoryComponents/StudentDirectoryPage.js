134395b397237df2f0dbccb38d4ee063facef0dd
unknown <nolanreedhill@gmail.com>
2023-05-24 16:14:11 -0400

    actual final student directory

--- school-app/src/components/StudentDirectoryComponents/StudentDirectoryPage.js
+++ school-app/src/components/StudentDirectoryComponents/StudentDirectoryPage.js
@@ -1,26 +1,8 @@
-import {useEffect, useState} from 'react';
+
 import SearchBar from './SearchBar';
 import './Directory.css';
 export default function StudentDirectory() {
 
-    // const [students, setStudents] = useState([])
-    // useEffect(() => {
-    //     getStudents()
-    // }, [])
-    
-    // useEffect(() => {
-    // }, [students])
-    // function getStudents() {
-    //     const studentCollectionRef = collection(db, 'students')
-    //     getDocs(studentCollectionRef)
-    //     .then(response => {
-    //         const students = response.docs.map(doc => ({
-    //             data: doc.data(), 
-    //             id: doc.id,
-    //         }))
-    //         setStudents(students)
-    //     }).catch(error => console.log(error.message))
-    // }
         return (
             <>
 
