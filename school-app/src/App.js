ed75acd5766352f3a28a55ad4f5a9e95ec0137cd
averydossman <averydossman@gmail.com>
2023-05-24 09:49:20 -0400

    shows class page header

--- school-app/src/App.js
+++ school-app/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
+import ClassPage from './components/ClassPage'; // adjust the path as needed
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+    <div className="App">
+      <header className="App-header">
+        <h1></h1>
       </header>
+      <main>
+        <ClassPage />
+      </main>
     </div>
   );
 }
