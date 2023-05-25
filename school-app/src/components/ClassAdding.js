4c4ae70dfafb02e0162fdcb8019035cff5d2b313
Alex Talreja <alextalreja@gmail.com>
2023-05-25 09:32:21 -0400

    linked teacher name to directory

--- school-app/src/components/ClassAdding.js
+++ school-app/src/components/ClassAdding.js
@@ -6,11 +6,12 @@ import {Button, TextField} from '@mui/material';
 function ClassAdding() {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   const [displayError, setDisplayError] = useState(false);
+  const [teacherError, setTeacherError] = useState(false);
   const [classData, setClassData] = useState({
     teacher: '',
     subject: '',
     grade: 0,
-    num: 0
+    num: 0,
   });
 
   const handlePopupOpen = () => {
@@ -19,6 +20,7 @@ function ClassAdding() {
 
   const handlePopupClose = () => {
     setDisplayError(false);
+    setTeacherError(false);
     setIsPopupOpen(false);
   };
 
@@ -32,7 +34,7 @@ function ClassAdding() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+  
     try {
       // Check if there is an existing class with the same name, subject, and grade
       const querySnapshot = await getDocs(collection(db, 'classes'));
@@ -44,26 +46,39 @@ function ClassAdding() {
           data.grade === classData.grade
         );
       });
-      
+  
       if (existingClass) {
         // Display an error message to the user
         console.log('Error: Class already exists with the same name, subject, and grade.');
         setClassData({ teacher: '', subject: '', grade: 0, num: 0 });
         setDisplayError(true);
-
       } else {
-        // Save classData to the database using Firebase Firestore
-        await addDoc(collection(db, 'classes'), classData);
-        // Reset the classData state
-        setClassData({ teacher: '', subject: '', grade: 0, num: 0 });
-        setDisplayError(false);
-        // Close the popup
-        setIsPopupOpen(false);
+        // Check if the entered teacher name exists in the "teachers" collection
+        const teacherQuerySnapshot = await getDocs(collection(db, 'teachers'));
+        const teacherDoc = teacherQuerySnapshot.docs.find((doc) => doc.data().name === classData.teacher);
+  
+        if (teacherDoc) {
+          // Save the class data with the teacher field as the Document ID of the teacher
+          const teacherId = teacherDoc.id;
+          const classDataWithTeacherId = { ...classData, teacher: teacherId, roster: [] };
+          await addDoc(collection(db, 'classes'), classDataWithTeacherId);
+          // Reset the classData state
+          setClassData({ teacher: '', subject: '', grade: 0, num: 0});
+          setDisplayError(false);
+          // Close the popup
+          setIsPopupOpen(false);
+        } else {
+          // Display an error message if the teacher doesn't exist
+          console.log('Error: Teacher does not exist.');
+          setClassData({ teacher: '', subject: '', grade: 0, num: 0 });
+          setTeacherError(true);
+        }
       }
     } catch (error) {
       console.log('Error adding class:', error);
     }
   };
+  
   
 
   return (
@@ -73,9 +88,6 @@ function ClassAdding() {
       {isPopupOpen && (
         <div className="popup">
           <div className="popup-content"> <br></br>
-            <span className="close" onClick={handlePopupClose}>
-              Close
-            </span>
             <form onSubmit={handleSubmit}>
               <TextField
                 label="Teacher"
@@ -118,9 +130,16 @@ function ClassAdding() {
                 sx={{ width: '200px' }}
                 margin="normal"
               />
-              <Button variant='contained' type="submit">Confirm</Button>
+              
+              
             </form>
+            
+            <Button varient='contained' className="close" onClick={handlePopupClose}>
+              Close
+            </Button>
+            <Button variant='contained' type="submit" onClick={handleSubmit}>Confirm</Button>
             {displayError && <p>Error: Class already exists with the same name, subject, and grade.</p>}
+            {teacherError && <p style={{ color: 'red', fontWeight: 'bold', fontFamily: 'Arial' }}>Error: That teacher does not exist in the directory.</p>}
           </div>
         </div>
       )}
--- school-app/src/components/ClassSearch.js
+++ school-app/src/components/ClassSearch.js
@@ -1,17 +1,27 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { db } from './Firebase';
 import ClassAdding from './ClassAdding';
-import {collection, doc, getDocs} from "firebase/firestore";
-import { Button, FormControl, MenuItem, Select, InputLabel, TextField, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
-
+import { collection, doc, getDocs, getDoc } from 'firebase/firestore';
+import {
+  Button,
+  FormControl,
+  MenuItem,
+  Select,
+  InputLabel,
+  TextField,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
 
 function ClassSearch() {
-
-    const [selectedGrade, setSelectedGrade] = useState('');
-    const [inputValue, setInputValue] = useState('');
-    const [classes, setClasses] = useState([]);
-    const [filteredClasses, setFilteredClasses] = useState([]);
-
+  const [selectedGrade, setSelectedGrade] = useState('');
+  const [inputValue, setInputValue] = useState('');
+  const [classes, setClasses] = useState([]);
+  const [filteredClasses, setFilteredClasses] = useState([]);
 
   const handleGradeChange = (event) => {
     setSelectedGrade(event.target.value);
@@ -23,7 +33,7 @@ function ClassSearch() {
 
   const handleSearch = () => {
     let filteredData = classes;
-  
+
     if (selectedGrade && selectedGrade !== '' && selectedGrade !== 'Select a grade') {
       filteredData = filteredData.filter((item) => {
         const itemGrade = typeof item.grade === 'string' ? parseInt(item.grade) : item.grade;
@@ -31,107 +41,108 @@ function ClassSearch() {
         return itemGrade === selectedGradeValue;
       });
     }
-  
+
     if (inputValue && inputValue !== '' && inputValue !== undefined) {
       filteredData = filteredData.filter((item) =>
-        item.teacher && item.teacher.toLowerCase().includes(inputValue.toLowerCase())
+        item.teacherName && item.teacherName.toLowerCase().includes(inputValue.toLowerCase())
       );
     }
-  
+
     setFilteredClasses(filteredData);
   };
-  
 
   useEffect(() => {
     const fetchDocs = async () => {
       try {
-        const classCollection = collection(db, "classes");
+        const classCollection = collection(db, 'classes');
         const querySnapshot = await getDocs(classCollection);
-        const data = querySnapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data()
-        }));
+        const data = [];
+
+        for (const docSnap of querySnapshot.docs) {
+          const docRef = doc(db, 'teachers', docSnap.data().teacher);
+          const teacherDocSnap = await getDoc(docRef);
+          const teacherName = teacherDocSnap.exists() ? teacherDocSnap.data().name : '';
+          const classData = {
+            id: docSnap.id,
+            ...docSnap.data(),
+            teacherName: teacherName,
+          };
+          data.push(classData);
+        }
+
         data.sort((a, b) => {
           if (a.grade !== b.grade) {
-            return a.grade - b.grade; 
+            return a.grade - b.grade;
           }
-          return a.teacher.localeCompare(b.teacher); 
+          return a.teacherName.localeCompare(b.teacherName);
         });
 
         setClasses(data);
         setFilteredClasses(data);
       } catch (error) {
-        console.error("Error fetching classes RIP OH NOOOOO ):", error);
+        console.error('Error fetching classes:', error);
       }
     };
 
     fetchDocs();
   }, []);
 
-  
-
   return (
     <div>
       <h2>TJES Class Search Page</h2>
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '16px'}}>
-      
-      <TextField id="outlined-basic" label="Teacher Last Name" variant="outlined" value={inputValue} onChange={handleNameChange} />
-      <FormControl>
-        <InputLabel>Grade</InputLabel>
-      <Select
-        labelId="gradelabel"
-        id="gradelabel"
-        value={selectedGrade}
-        label="Grade"
-        onChange={handleGradeChange}
-        style={{ width: '200px' }}
-      >
-        <MenuItem value={0}>Any</MenuItem>
-        <MenuItem value={1}>1</MenuItem>
-        <MenuItem value={2}>2</MenuItem>
-        <MenuItem value={3}>3</MenuItem>
-        <MenuItem value={4}>4</MenuItem>
-        <MenuItem value={5}>5</MenuItem>
-      </Select>
-      </FormControl>
-      <Button onClick = {handleSearch}variant="contained" style={{ height: '56px' }}>Search</Button>
-    </div>
-    <ClassAdding />
-
-    
-    
-      
-     
-
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '16px' }}>
+        <TextField id="outlined-basic" label="Teacher Last Name" variant="outlined" value={inputValue} onChange={handleNameChange} />
+        <FormControl>
+          <InputLabel>Grade</InputLabel>
+          <Select
+            labelId="gradelabel"
+            id="gradelabel"
+            value={selectedGrade}
+            label="Grade"
+            onChange={handleGradeChange}
+            style={{ width: '200px' }}
+          >
+            <MenuItem value={0}>Any</MenuItem>
+            <MenuItem value={1}>1</MenuItem>
+            <MenuItem value={2}>2</MenuItem>
+            <MenuItem value={3}>3</MenuItem>
+            <MenuItem value={4}>4</MenuItem>
+            <MenuItem value={5}>5</MenuItem>
+          </Select>
+        </FormControl>
+        <Button onClick={handleSearch} variant="contained" style={{ height: '56px' }}>
+          Search
+        </Button>
+      </div>
+      <ClassAdding />
       <TableContainer>
-  <Table>
-    <TableHead>
-      <TableRow>
-        <TableCell style={{ fontWeight: 'bold' }}><Button>Teacher</Button></TableCell>
-        <TableCell style={{ fontWeight: 'bold' }}>Subject</TableCell>
-        <TableCell style={{ fontWeight: 'bold' }}>Grade Level</TableCell>
-        <TableCell></TableCell>
-      </TableRow>
-    </TableHead>
-    <TableBody>
-      {filteredClasses.map((poll) => (
-        <TableRow key={poll.id}>
-          <TableCell>{poll.teacher}</TableCell>
-          <TableCell>{poll.subject}</TableCell>
-          <TableCell>{poll.grade}</TableCell>
-          <TableCell><button>More Info</button></TableCell>
-        </TableRow>
-      ))}
-    </TableBody>
-  </Table>
-</TableContainer>
-      
-
-      
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell style={{ fontWeight: 'bold' }}>
+                <Button>Teacher</Button>
+              </TableCell>
+              <TableCell style={{ fontWeight: 'bold' }}>Subject</TableCell>
+              <TableCell style={{ fontWeight: 'bold' }}>Grade Level</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {filteredClasses.map((classData) => (
+              <TableRow key={classData.id}>
+                <TableCell>{classData.teacherName}</TableCell>
+                <TableCell>{classData.subject}</TableCell>
+                <TableCell>{classData.grade}</TableCell>
+                <TableCell>
+                  <button>More Info</button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
-
-
 }
 
 export default ClassSearch;
